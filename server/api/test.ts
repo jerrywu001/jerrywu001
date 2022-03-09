0342e20744a9a9c801e70626b6f8a326c008f6ba
jerrywu <jerrywu001@gmail.com>
2022-03-09 09:52:42 +0800

    api test

--- server/api/test.ts
+++ server/api/test.ts
@@ -3,24 +3,15 @@ import fs from 'fs';
 import path from 'path';
 
 async function main(req: IncomingMessage, res: ServerResponse) {
-  const filePath0 = await path.resolve('./docs/vue/test.md');
-  const filePath1 = await path.resolve('docs/vue/test.md');
-  const filePath2 = await path.resolve('../docs/vue/test.md');
-  const filePath3 = await path.resolve('../../docs/vue/test.md');
-  const filePath4 = await path.resolve('../../../docs/vue/test.md');
+  const query = new URLSearchParams(req.url);
+  let url = query.get('url');
+  url = url || './docs/vue/test.md';
+  const filePath0 = await path.resolve(url);
 
   return {
     filePath0,
-    filePath1,
-    filePath2,
-    filePath3,
-    filePath4,
     stat: {
       f0: fs.existsSync(filePath0),
-      f1: fs.existsSync(filePath1),
-      f2: fs.existsSync(filePath2),
-      f3: fs.existsSync(filePath3),
-      f4: fs.existsSync(filePath4),
     },
   };
 }
