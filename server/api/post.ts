9d62db930677e26662391ead58240c5e867d65f4
jerrywu <jerrywu001@gmail.com>
2022-03-16 19:17:59 +0800

    update

--- server/api/post.ts
+++ server/api/post.ts
@@ -1,23 +1,38 @@
 import type { IncomingMessage, ServerResponse } from 'http';
 import fetch from 'node-fetch';
+import chalk from 'chalk';
+
+const protocol = process.env.API_HTTPS === 'false' ? 'http:' : 'https:';
+const host = process.env.API_HOST;
+const port = process.env.API_PORT;
 
 export default async (req: IncomingMessage, res: ServerResponse) => {
-  const rawHeaders = req.rawHeaders || [];
+  let body = {} as any;
+
   const query = new URLSearchParams(req.url);
   const category = query.get('category');
   const postname = query.get('postname');
 
-  const fullPath = rawHeaders.find(
-    (v) => v && v.startsWith('http') && v.includes('/posts')
-  );
-  const path = fullPath ? fullPath.split('/posts')[0] : 'http://localhost:3000';
+  const path = `${protocol}//${host}${
+    String(port) === '80' ? '' : `:${String(port)}`
+  }`;
 
-  const catlogs = await fetch(`${path}/content/categories.json`);
-  const response = await fetch(`${path}/content/${category}-${postname}.json`);
-  const body: any = await response.json();
-  const categories = await catlogs.json();
-  if (body && categories) {
-    body.categories = categories;
+  console.info(chalk.blue(`fullPath: ${path}`));
+
+  try {
+    const queryUrl = `${path}/content/${category}-${postname}.json`;
+    const catlogs = await fetch(`${path}/content/categories.json`);
+    const categories = await catlogs.json();
+    const response = await fetch(queryUrl);
+
+    console.info(chalk.green(`path invoked: ${queryUrl}`));
+
+    body = await response.json();
+    if (body && categories) {
+      body.categories = categories;
+    }
+  } catch (error) {
+    console.error(error);
   }
   return body;
 };
