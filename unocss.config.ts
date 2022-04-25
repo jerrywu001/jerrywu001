79e375271df965229a0d569b27181a3223547968
wuchao <wuchao@gmail.com>
2022-04-25 20:53:14 +0800

    test ci

--- unocss.config.ts
+++ unocss.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'unocss';
 import presetAttributify from '@unocss/preset-attributify';
 import presetIcons from '@unocss/preset-icons';
 import presetWebFonts from '@unocss/preset-web-fonts';
+import { presetUno } from '@unocss/preset-uno';
 
 export default defineConfig({
   presets: [
+    presetUno(),
     presetAttributify(),
     // https://icones.js.org/
     // http://localhost:3000/__unocss
