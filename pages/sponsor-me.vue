5bc958ce0d588857786cd2999081fad1db6f0da5
jerrywu <jerrywu001@gmail.com>
2023-09-25 17:21:53 +0800

    docs: payment

---
+++ pages/sponsor-me.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="w-[400px] mx-auto p-6 overflow-x-hidden">
+    <div class="text-lg leading-1 py-6 font-bold">Alipay</div>
+    <div>
+      <img src="/alipay.jpg" class="w-full" alt="alipay" />
+    </div>
+    <div class="text-lg leading-1 py-6 font-bold mt-3">Wechat</div>
+    <div>
+      <img src="/wechat.jpg" class="w-full" alt="wechat" />
+    </div>
+  </div>
+</template>
